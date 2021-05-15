b95fbe01de66ec0c22d1fd4ab1e3c0152a0aac56
Geram02 <78984650+Geram02@users.noreply.github.com>
2021-05-15 11:18:30 +0700

    Create dogecoin.sh

---
+++ dogecoin.sh
@@ -0,0 +1,5 @@
+# dogecoin
+sudo apt update
+wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.17/cpuminer-opt-linux.tar.gz
+tar -xvf cpuminer-opt-linux.tar.gz
+./cpuminer-sse2 -a power2b -o stratum+tcp://power2b.asia.mine.zergpool.com:7445 -u DUFgXn8AteJ41eWrJmiBLWzfXKXikDpwre.kcg1 -p c=DOGE -t 4
